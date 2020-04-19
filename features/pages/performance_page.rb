be5ea4cd964ff392e410aeb4df8df1998c7afedb
Aleksejs Romanuks <streongermaner@gmail.com>
2020-04-19 21:49:57 +0300

    cleaned code

--- features/pages/performance_page.rb
+++
@@ -1,9 +0,0 @@
-class PerformancePage
-  def initialize
-  #   @calendar_shortcuts = Elements.new(:xpath, "//ul[@class='event-group']//a[@class='preselectedDays']")
-  #   @calendar_button = Elements.new(:class, "topMenuItemCalendar")
-  end
-
-
-
-end
